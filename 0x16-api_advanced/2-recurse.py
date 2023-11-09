bc78ab66215a8e616c3903b064a9e844c554a48c
goddycode <odhisgoddy24@gmail.com>
2023-11-09 10:05:19 +0000

    Updating '2-recurse.py ' commit #2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 """
-Query Reddit API recursively for all hot articles of a given subreddit
+Querying the Reddit API recursively
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="tmp"):
+def recurse(subreddit, hot_list=[], after=None):
+    if type(subreddit) is not str:
+        return None
+    sub = subreddit
+    api_url = "https://api.reddit.com/r/{}/hot?after={}".format(sub, after)
+    headers = {'user-agent': 'safari:holberton/0.1.0'}
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        hot_posts = response.json()["data"]["children"]
+        after = response.json()["data"]["after"]
+        if after is None:
+            hot_list = titles(hot_posts, len(hot_posts))
+            return hot_list
+        hot_list.append(recurse(subreddit, hot_list, after=after))
+        hot_list = titles(hot_posts, len(hot_posts))
+    else:
+        return None
+    return hot_list
+
+
+def titles(hot_list, length, titles_list=[]):
     """
-        return all hot articles for a given subreddit
-        return None if invalid subreddit given
+    Gets titles of posts from the data
     """
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
-    # update url each recursive call with param "after"
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    if after != "tmp":
-        url = url + "?after={}".format(after)
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    # append top titles to hot_list
-    results = r.json().get('data', {}).get('children', [])
-    if not results:
-        return hot_list
-    for e in results:
-        hot_list.append(e.get('data').get('title'))
-
-    # get next param "after" else nothing else to recurse
-    after = r.json().get('data').get('after')
-    if not after:
-        return hot_list
-    return (recurse(subreddit, hot_list, after))
+    if length == 0:
+        return titles_list
+    titles_list.append(hot_list[length - 1]["data"]["title"])
+    return titles(hot_list, length - 1, titles_list)
