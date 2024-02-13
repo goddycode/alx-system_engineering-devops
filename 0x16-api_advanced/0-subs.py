2636904af7b814c72e752b47a8c41e2f344277c7
goddycode <odhisgoddy24@gmail.com>
2024-02-13 14:20:12 +0000

    '0-subs.py' update #2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 """
- Method which queries the Reddit API
- and returns the number of subscribers
+Method which queries the Reddit API
+and returns the number of subscribers
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """initializate"""
-    if (type(subreddit) is not str):
-        return(0)
-    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
-    headers = {'user-agent': 'safari:holberton/0.1.0'}
-    response = requests.get(url_api, headers=headers)
-    if response.status_code is not 200:
-        return(0)
-    return(response.json().get("data").get("subscribers"))
+    """
+    Returns the number of subscribers for a given subreddit.
+    :param subreddit: The name of the subreddit as a string.
+    :return: The number of subscribers (int). Returns 0 if subreddit is invalid or an error occurs.
+    """
+    if not isinstance(subreddit, str):
+        return 0
+    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'safari:holberton/0.1.0'}
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        return 0
+    
+    try:
+        data = response.json()
+        return data['data']['subscribers']
+    except (KeyError, ValueError):
+        return 0
+
