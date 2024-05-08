85d06f8bc0348728f7c154475e6b3f072c566f8c
goddycode <odhisgoddy24@gmail.com>
2024-05-08 01:20:14 +0000

    Updating '0-subs.py'file commit #6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
-def get_number_of_subscribers(subreddit_name):
-  """
-  Fetches the number of subscribers for a given subreddit using Reddit's API.
-
-  Args:
-      subreddit_name: The name of the subreddit (string).
-
-  Returns:
-      The number of subscribers (integer) or 0 if an error occurs.
-  """
-
-  url = f'https://www.reddit.com/r/{subreddit_name}/about.json'
- 
-  headers = {'User-Agent': 'your_app_name v1.0.0 (by /u/your_username)'}
-
-  try:
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    response.raise_for_status()
-
-    return data.get("data", {}).get("subscribers", 0)
-
-  except requests.exceptions.RequestException as e:
-  
-    print(f"Error fetching subscriber count: {e}")
-    return 0
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
