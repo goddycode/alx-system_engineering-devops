1e9f4f723a9129c4e003aebd09f60f2561934ccf
goddycode <odhisgoddy24@gmail.com>
2024-05-08 00:51:36 +0000

    Updating '0-subs.py'file commit #3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,15 @@
-#!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+"
+    Return the total number of subscribers on a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise exception for non-200 status codes
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
         return 0
